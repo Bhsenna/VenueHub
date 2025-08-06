37bfe0d54c478304f949b7eba3159f9a9ff0fc17
Caio Abraao <caio.abraao@senior.com.br>
2025-08-06 17:33:37 -0300

    Criando endpoint createAdditional

--- api/src/main/java/venue/hub/api/domain/dtos/additional/AdditionalRequestDTO.java
+++ api/src/main/java/venue/hub/api/domain/dtos/additional/AdditionalRequestDTO.java
@@ -1,7 +1,16 @@
 package venue.hub.api.domain.dtos.additional;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdditionalRequestDTO {
 
+    @NotBlank
     private String nome;
 
 }
