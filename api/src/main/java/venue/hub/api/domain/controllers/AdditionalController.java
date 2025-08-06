c5ee4fc5de3074c63308a0ce74faed0270e88be9
Caio Abraao <caio.abraao@senior.com.br>
2025-08-06 17:55:35 -0300

    Criando endpoint getAllAdditionals

--- api/src/main/java/venue/hub/api/domain/controllers/AdditionalController.java
+++ api/src/main/java/venue/hub/api/domain/controllers/AdditionalController.java
@@ -2,14 +2,16 @@ package venue.hub.api.domain.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import venue.hub.api.domain.dtos.additional.AdditionalRequestDTO;
 import venue.hub.api.domain.dtos.additional.AdditionalResponseDTO;
+import venue.hub.api.domain.dtos.page.PageResponse;
 import venue.hub.api.domain.services.AdditionalService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/additionals")
@@ -25,4 +27,19 @@ public class AdditionalController {
         return ResponseEntity.ok(created);
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<PageResponse<AdditionalResponseDTO>> getAllAdditionals(
+            @PageableDefault(size = 10, sort = {"id"}) Pageable paginacao
+    ) {
+        var additionalPage = additionalService.getAllAdditionals(paginacao);
+        List<AdditionalResponseDTO> additionals = additionalPage.getContent();
+
+        return ResponseEntity.ok(
+                PageResponse.<AdditionalResponseDTO>builder()
+                        .totalPages(additionalPage.getTotalPages())
+                        .totalElements(additionalPage.getTotalElements())
+                        .currentPageData(additionals)
+                        .build()
+        );
+    }
 }
