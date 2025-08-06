36045e5c289b92479c48c3014ed493d2ec4161d5
Caio Abraao <caio.abraao@senior.com.br>
2025-08-06 17:33:37 -0300

    Criando endpoint createAdditional

---
+++ api/src/main/java/venue/hub/api/domain/dtos/mapper/AdditionalMapper.java
@@ -0,0 +1,14 @@
+package venue.hub.api.domain.dtos.mapper;
+
+import org.mapstruct.Mapper;
+import venue.hub.api.domain.dtos.additional.AdditionalRequestDTO;
+import venue.hub.api.domain.dtos.additional.AdditionalResponseDTO;
+import venue.hub.api.domain.entities.Additional;
+
+@Mapper(componentModel = "spring")
+public interface AdditionalMapper {
+
+    AdditionalResponseDTO toDTO(Additional additional);
+
+    Additional toEntity(AdditionalRequestDTO additionalRequestDTO);
+}
