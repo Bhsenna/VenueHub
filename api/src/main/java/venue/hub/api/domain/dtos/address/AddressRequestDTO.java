1619ca721dccff0d53803ee44d31b87d1e197273
Caio Abraao <caio.abraao@senior.com.br>
2025-08-06 16:27:53 -0300

    Adicionando validações para User e Address RequestDTO

--- api/src/main/java/venue/hub/api/domain/dtos/address/AddressRequestDTO.java
+++ api/src/main/java/venue/hub/api/domain/dtos/address/AddressRequestDTO.java
@@ -1,5 +1,9 @@
 package venue.hub.api.domain.dtos.address;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +14,23 @@ import venue.hub.api.domain.enums.Estado;
 @NoArgsConstructor
 public class AddressRequestDTO {
 
+    @NotNull @Pattern(regexp = "\\d{8}")
     private String cep;
+    @NotBlank
     private String logradouro;
-    private int numero;
+    @NotNull @Positive
+    private Integer numero;
+    @NotBlank
     private String complemento;
+    @NotBlank
     private String bairro;
+    @NotBlank
     private String cidade;
+    @NotNull
     private Estado estado;
+    @NotNull
     private Double latitude;
+    @NotNull
     private Double longitude;
 
 }
