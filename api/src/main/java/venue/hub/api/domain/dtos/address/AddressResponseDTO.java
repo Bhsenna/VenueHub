87435c32d82b98afed6399f9f2189ef5e987c9f5
Bernardo Hazime <bernardo.hazime@senior.com.br>
2025-08-06 14:53:30 -0300

    Implementado AddressResponseDTO, adicionado a UserResponseDTO

--- api/src/main/java/venue/hub/api/domain/dtos/address/AddressResponseDTO.java
+++ api/src/main/java/venue/hub/api/domain/dtos/address/AddressResponseDTO.java
@@ -1,4 +1,22 @@
 package venue.hub.api.domain.dtos.address;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import venue.hub.api.domain.enums.Estado;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddressResponseDTO {
+    private Long id;
+    private String cep;
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private Estado estado;
+    private Double latitude;
+    private Double longitude;
 }
