024b58d8c4ec4b8bcd98274fb9301efa57ed45f3
Caio Abraao <caio.abraao@senior.com.br>
2025-08-07 10:03:39 -0300

    #GODEV-1106 criando Additional endpoint/service getById e update

--- api/src/main/java/venue/hub/api/domain/services/AdditionalService.java
+++ api/src/main/java/venue/hub/api/domain/services/AdditionalService.java
@@ -1,8 +1,10 @@
 package venue.hub.api.domain.services;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import venue.hub.api.domain.dtos.additional.AdditionalRequestDTO;
@@ -10,6 +12,7 @@ import venue.hub.api.domain.dtos.additional.AdditionalResponseDTO;
 import venue.hub.api.domain.dtos.mapper.AdditionalMapper;
 import venue.hub.api.domain.entities.Additional;
 import venue.hub.api.domain.repositories.AdditionalRepository;
+import venue.hub.api.infra.exceptions.AdditionalNotFoundException;
 
 @Service
 public class AdditionalService {
@@ -33,4 +36,20 @@ public class AdditionalService {
                 .map(additionalMapper::toDTO);
     }
 
+    public AdditionalResponseDTO getAdditionalById(Long id) {
+        var additional = this.findById(id);
+        return additionalMapper.toDTO(additional);
+    }
+
+    private Additional findById(Long id) {
+        return additionalRepository.findById(id)
+                .orElseThrow(() -> new AdditionalNotFoundException("Adicional não encontrado com o id: " + id, HttpStatus.NOT_FOUND));
+    }
+
+    public AdditionalResponseDTO updateAdditional(Long id, @Valid AdditionalRequestDTO dto) {
+        var additional = this.findById(id);
+        additional.update(dto);
+        additionalRepository.save(additional);
+        return additionalMapper.toDTO(additional);
+    }
 }
