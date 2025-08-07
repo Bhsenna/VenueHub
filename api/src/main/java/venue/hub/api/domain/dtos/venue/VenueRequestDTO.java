b5e1c8f27568b030924fb3170c419905b84c6379
Gabriella Bento <gabriella.bento@senior.com.br>
2025-08-07 10:08:55 -0300

    criando endpoints atualizar e deletar Venue

--- api/src/main/java/venue/hub/api/domain/dtos/venue/VenueRequestDTO.java
+++ api/src/main/java/venue/hub/api/domain/dtos/venue/VenueRequestDTO.java
@@ -1,23 +1,38 @@
 package venue.hub.api.domain.dtos.venue;
 
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import venue.hub.api.domain.dtos.address.AddressRequestDTO;
-import venue.hub.api.domain.dtos.user.UserRequestDTO;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class VenueRequestDTO {
 
+    @NotNull
     private String nome;
-    private int capacidade;
+
+    @NotNull
+    @Positive
+    private Integer capacidade;
     private String descricao;
+
+    @NotNull
+    @Pattern(regexp = "^\\(?[1-9]{2}\\)? ?(?:[2-8]|9[1-9])[0-9]{3}-?[0-9]{4}$")
     private String telefone;
+
+    @Positive
     private double valor;
+
+    @NotNull
     private AddressRequestDTO address;
+
+    @NotNull
     private Long userId;
 
 }
