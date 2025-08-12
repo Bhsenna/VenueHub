e68d7559b8aff0216e782fa887ea3450a4e7c05a
Gabriella Bento <gabriella.bento@senior.com.br>
2025-08-12 16:38:23 -0300

    GODEV-1189 adiciona menu e event-list

--- venuehub-angular/src/app/app.routes.ts
+++ venuehub-angular/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { Login } from './components/forms/login/login';
 import { Register } from './components/forms/register/register';
 import { Home } from './components/pages/home/home';
-import { VenueDetails } from './components/venue-details/venue-details';
+import { EventList } from './components/list/event-list/event-list';
+import { VenueDetails } from './components/pages/venue-details/venue-details';
+import { EventDetails } from './components/pages/event-details/event-details';
 
 export const routes: Routes = [
     { path: '', component: Login },
     { path: 'register', component: Register },
     { path: 'home', component: Home },
-    { path: 'venues/:id', component: VenueDetails }
+    { path: 'venues/:id', component: VenueDetails },
+    { path: 'events', component: EventList},
+    { path: 'events/:id', component: EventDetails }
+
 ];
