a0bb0c4b5e173d202fc6e8315c295b64babbae04
Gabriella Bento <gabriella.bento@senior.com.br>
2025-08-12 16:37:24 -0300

    Adiciona endpoints de getEventDetails e getProposalByEventId

--- api/src/main/java/venue/hub/api/domain/dtos/mapper/EventMapper.java
+++ api/src/main/java/venue/hub/api/domain/dtos/mapper/EventMapper.java
@@ -2,15 +2,19 @@ package venue.hub.api.domain.dtos.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import venue.hub.api.domain.dtos.event.EventDetailsDTO;
 import venue.hub.api.domain.dtos.event.EventRequestDTO;
 import venue.hub.api.domain.dtos.event.EventResponseDTO;
 import venue.hub.api.domain.entities.Event;
+import venue.hub.api.domain.enums.Status;
 import venue.hub.api.domain.services.UserService;
 
 @Mapper(componentModel = "spring", uses = {UserService.class, UserMapper.class})
 public interface EventMapper {
 
     EventResponseDTO toDTO(Event event);
+
+    EventDetailsDTO toDTO(EventResponseDTO response, Long venueId, Status status);
 
     @Mapping(target = "additionals", ignore = true)
     Event toEntity(EventRequestDTO eventRequestDTO);
